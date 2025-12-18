4274dcb390840124361b221f96790babb3b05c44
Samuel Ming <samuelming09@gmail.com>
2025-12-18 12:03:37 +0100

    Initial Commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,6 @@
+<x-layout>
+    <h1>Über Mich</h1>
+    <p>Willkommen auf meiner Über Mich Seite! Hier erfahren Sie mehr über mich und meine Hintergründe.</p>
+</x-layout>
+
+
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,4 @@
+<x-layout>
+    <h1>Kontakt</h1>
+    <p>Willkommen auf meiner Kontaktseite</p>
+</x-layout>
