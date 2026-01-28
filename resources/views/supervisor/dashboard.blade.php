0be396dbf3d875738ebb9e855237d1e5fd17fd75
Samuel Ming <samuelming09@gmail.com>
2026-01-28 20:44:06 +0100

    Update Supervisor dashboard: modify task progress labels and add tooltip for active tasks

--- resources/views/supervisor/dashboard.blade.php
+++ resources/views/supervisor/dashboard.blade.php
@@ -108,9 +108,9 @@
                         </div>
 
                         <div class="mt-2">
-                            <div class="flex justify-between text-xs font-semibold mb-1">
-                                <span>Fortschritt</span>
-                                <span>{{ $completedTasks }} / {{ $totalTasks }} Aufgaben</span>
+                            <div class="flex justify-between text-xs font-semibold mb-1 text-gray-400">
+                                <span>Aktive Aufgaben</span>
+                                <span title="Nur Aufgaben aus nicht abgeschlossenen Kursen">{{ $completedTasks }} / {{ $totalTasks }}</span>
                             </div>
                             <div class="w-full bg-gray-200 rounded-full h-2">
                                 <div class="bg-green-500 h-2 rounded-full transition-all duration-500" style="width: {{ $percentage }}%"></div>
